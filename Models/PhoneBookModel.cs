fbed6157caf607c53d5c2111b2e951575cc7fad6
Olta DEDEJ_EXT <olta.dedej_ext@alten.it>
2020-12-04 11:59:04 +0100

    first commit

---
+++ Models/PhoneBookModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfTIK.Models
+{
+    [DataContract]
+    [Serializable]
+    public class PhoneBookModel
+    {
+        [DataMember]
+        public long? id { get; set; }
+
+        [DataMember]
+        public string firstName { get; set; }  //pascal case --> camel case
+
+
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using Phone_Book_DB.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfTIK
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private readonly PhoneBookServicesDb servicesDb = new PhoneBookServicesDb();
+        public string GetData(int value)
+        {
+            if (servicesDb.IsConnected())
+            { 
+
+                return string.Format("You entered: {0}", value);
+            }
+
+            return "Probleme ";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
